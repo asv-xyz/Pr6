42fed5608241c685f0ff59cf11bdc8c75ebaa96a
asv-xyz <alex.sukhorukov123@yandex.ru>
2025-11-17 11:54:34 +0300

    Minor changes
    Added some variables
    Added parsing commands from terminal
    Added some functionality to main
    Next thing to do: add functions

--- script.sh
+++ script.sh
@@ -1,16 +1,48 @@
 #!/bin/bash
 
-while getopts ":uphl:e:" opt; do
-	case $opt in
-		u)
-			echo "1" ;;
-		p)
-			echo "2" ;;
-		h)
-			echo "3" ;;
-		l)
-			echo "4" ;;
-		e)
-			echo "5" ;;
-	esac
+# Variables
+OPTIONS=uphl:e:
+LONGOPTS=users,processes,help,log:,errors:
+PARSED=`getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@"`
+
+# Check if any errors occured
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+# Format
+eval set -- "$PARSED"
+
+# Main script
+while true; do
+    case "$1" in
+        -u|--users)
+            echo "1"
+            shift
+            ;;
+        -p|--processes)
+            echo "2"
+            shift
+            ;;
+        -h|--help)
+            echo "3"
+            shift
+            ;;
+        -l|--logfile)
+            echo "4: $2"
+            shift 2
+            ;;
+        -e|--errors)
+            echo "5: $2"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Internal error"
+            exit 1
+            ;;
+    esac
 done
